4425fc3437b779aace597729356b18b1dff2dae7
VardanHeroic <tarayanvardan@gmail.com>
2024-07-28 11:42:59 +0400

    translate text to morse

--- src/MorseInput.tsx
+++ src/MorseInput.tsx
@@ -1,15 +1,33 @@
+import mapping from './mapping.json'
+
 interface ChildProps {
     setText: React.Dispatch<React.SetStateAction<string>>;
     text: string;
 }
 
 const MorseInput: React.FC<ChildProps> = ({ setText, text }) => {
+    const getKeyByValue = (value:string) => {
+        return Object.keys(mapping).find(key => mapping[key as keyof Object].toString() === value);
+    };
+
     function handleChange(e:React.ChangeEvent<HTMLInputElement>) {
-        setText(e.target.value)
+        // console.log(e.target.value.split(' ').map(morse => getKeyByValue(morse)));
+        // e.target.value.split(' ').map(morse => console.log(morse))
+        const morse = text.replace(/\s+/g, ' ').toUpperCase().split('').map(char => mapping[char as keyof Object]).join(' ')
+        const result = e.target.value.split(' ').map(morse => getKeyByValue(morse)).join('')
+        if (result[0]) {
+            setText(result)
+            e.target.value = morse
+        }
+        else{
+            setText('#')
+        }
     }
+
+
     return (
         <div>
-            <input type="text" onInput={handleChange} value={text} />
+            <input type="text" onInput={handleChange}  />
         </div>
     )
 }
