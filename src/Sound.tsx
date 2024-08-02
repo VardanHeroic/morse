2dc8a095a4a0bb04b5e2e740177249987d82ce58
VardanHeroic <tarayanvardan@gmail.com>
2024-08-02 09:51:17 +0400

    sliders work

--- src/Sound.tsx
+++ src/Sound.tsx
@@ -1,25 +1,26 @@
+import { useState } from "react"
+
 export default function Sound({ morse }: { morse: string }) {
-    let unit = 50
-    let freq = 1000
+    const [unit, setUnit] = useState(55)
+    const [freq, setFreq] = useState(550)
     const timing = {
         ' ': [3, 0],
         '.': [1, 1],
         '-': [3, 1],
-        '/': [1, 0]
+        '/': [0, 0]
     }
 
     const playSound = async (time: number, freq: number) => {
         return new Promise((resolve) => {
             const audioCtx = new window.AudioContext();
             const oscillator = audioCtx.createOscillator();
-            oscillator.type = 'sine'; // You can change this to 'square', 'sawtooth', or 'triangle'
-            oscillator.frequency.setValueAtTime(freq, audioCtx.currentTime); // Frequency in Hz
-            oscillator.connect(audioCtx.destination);
+            oscillator.type = 'sine'
+            oscillator.frequency.setValueAtTime(freq, audioCtx.currentTime);
+            oscillator.connect(audioCtx.destination)
             oscillator.start();
 
-            // Stop the sound after 1 second
             setTimeout(() => {
-                oscillator.stop();
+                oscillator.stop(audioCtx.currentTime);
                 resolve('')
             }, time)
         })
@@ -29,7 +30,7 @@ export default function Sound({ morse }: { morse: string }) {
     const playMorse = async () => {
         for (const char of morse.split('')) {
             if (char !== ' ') {
-            await playSound(unit,0)
+                await playSound(unit, 0)
             }
             await playSound(timing[char as keyof object][0] * unit, timing[char as keyof object][1] * freq)
         }
@@ -38,8 +39,8 @@ export default function Sound({ morse }: { morse: string }) {
     console.log(morse.split('').map(char => timing[char as keyof object]));
     return (
         <div>
-            <input type="range" min={100} value={1000} max={10000} />
-            <input type="range" min={50} value={100} max={1000} />
+            <input type="range" onInput={(e: any) => { setFreq(e.target.value) }} min={300} value={freq} max={800} />
+            <input type="range" onInput={(e: any) => { setUnit(e.target.value) }} min={20} value={unit} max={200} />
             <button onClick={playMorse}>START</button>
         </div>
     )
