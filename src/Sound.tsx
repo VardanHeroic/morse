708ed6eeabccc570cdedef4c9f8f2af6a8447e35
VardanHeroic <tarayanvardan@gmail.com>
2024-08-03 09:42:32 +0400

    now can stop sound

--- src/Sound.tsx
+++ src/Sound.tsx
@@ -1,8 +1,10 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export default function Sound({ morse }: { morse: string }) {
     const [unit, setUnit] = useState(55)
     const [freq, setFreq] = useState(550)
+    const [play, setPlay] = useState(false)
+    let stop = false
     const timing = {
         ' ': [3, 0],
         '.': [1, 1],
@@ -10,22 +12,30 @@ export default function Sound({ morse }: { morse: string }) {
         '/': [0, 0]
     }
 
+    const audioCtx = new window.AudioContext();
+    const oscillator = audioCtx.createOscillator();
+    oscillator.type = 'sine'
+    oscillator.start();
     const playSound = async (time: number, freq: number) => {
         return new Promise((resolve) => {
-            const audioCtx = new window.AudioContext();
-            const oscillator = audioCtx.createOscillator();
-            oscillator.type = 'sine'
+            if (stop) {
+                return;
+            }
             oscillator.frequency.setValueAtTime(freq, audioCtx.currentTime);
             oscillator.connect(audioCtx.destination)
-            oscillator.start();
-
             setTimeout(() => {
-                oscillator.stop(audioCtx.currentTime);
+                oscillator.disconnect(audioCtx.destination)
                 resolve('')
             }, time)
         })
             ;
     };
+
+    useEffect(() => {
+        if (play) {
+           playMorse()
+        }
+    },[play])
 
     const playMorse = async () => {
         for (const char of morse.split('')) {
@@ -34,14 +44,15 @@ export default function Sound({ morse }: { morse: string }) {
             }
             await playSound(timing[char as keyof object][0] * unit, timing[char as keyof object][1] * freq)
         }
+        stop = false
+        setPlay(false)
     }
 
-    console.log(morse.split('').map(char => timing[char as keyof object]));
     return (
-        <div>
-            <input type="range" onInput={(e: any) => { setFreq(e.target.value) }} min={300} value={freq} max={800} />
-            <input type="range" onInput={(e: any) => { setUnit(e.target.value) }} min={20} value={unit} max={200} />
-            <button onClick={playMorse}>START</button>
+        <div className="sound">
+            <input type="range" onInput={(e: any) => { setFreq(e.target.value) }} min={300} value={freq} max={800} /><span>{freq}</span><br/>
+            <input type="range" onInput={(e: any) => { setUnit(e.target.value) }} min={20} value={unit} max={200} /><span>{unit}</span><br/>
+            <button onClick={!play ? () => {setPlay(true)} : () => {setPlay(false); stop = true}}>{play ? "Stop" : "Play"}</button>
         </div>
     )
 }
